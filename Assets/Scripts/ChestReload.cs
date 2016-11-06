bd91153b81ee9143efea1d4ebd20f1810e3e8fb0
yulcat <alszmtm@gmail.com>
2016-11-06 20:04:05 +0900

    헬파이어 샷건 프리팹 따로 만들고 충돌판정 추가

---
+++ Assets/Scripts/ChestReload.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestReload : MonoBehaviour {
+	public Transform head;
+	Vector3 deltaPosition; 
+
+	// Use this for initialization
+	void Start () {
+		deltaPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.rotation = Quaternion.Euler(0,head.rotation.eulerAngles.y,0);
+		transform.position = head.position + deltaPosition + transform.forward * -0.1f;
+	}
+}
